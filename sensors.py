74e9075ffcca84189493700931d005ae1f61c75c
Brian Nguyen <haikunguyen@yahoo.com>
2019-04-09 18:00:54 +0100

    adding individual modules for sensors and combined script

---
+++ sensors.py
@@ -0,0 +1,79 @@
+# This python script combines the functionality of the magnetometer and the temperature, air pressure, humidity sensor
+
+import time
+
+import board
+import busio
+import adafruit_bme280
+
+import smbus
+
+#Magnetometer code
+# Get I2C bus
+bus = smbus.SMBus(1)
+# MLX90393 address, 0x0C(12)
+# Select write register command, 0x60(96)
+# AH = 0x00, AL = 0x5C, GAIN_SEL = 5, Address register (0x00 << 2)
+config = [0x00, 0x5C, 0x00]
+bus.write_i2c_block_data(0x0C, 0x60, config)
+# Read data back, 1 byte
+# Status byte
+data = bus.read_byte(0x0C)
+# MLX90393 address, 0x0C(12)
+# Select write register command, 0x60(96)
+# AH = 0x02, AL = 0xB4, RES for magnetic measurement = 0, Address register (0x02 << 2)
+config = [0x02, 0xB4, 0x08]
+bus.write_i2c_block_data(0x0C, 0x60, config)
+# Read data back, 1 byte
+# Status byte
+data = bus.read_byte(0x0C)
+# MLX90393 address, 0x0C(12)
+
+
+#Temperature, Air pressure, humidity sensor code
+#create library object using our Bus SPI port
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+bme_cs = digitalio.DigitalInOut(board.D10)
+bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
+
+#change this to match the location's pressure (hPa) at sea level
+bme280.sea_level_pressure = 1013.25
+
+while True:
+	# Start single meaurement mode, X, Y, Z-Axis enabled
+	bus.write_byte(0x0C, 0x3E)
+	# Read data back, 1 byte
+	# Status byte
+	data = bus.read_byte(0x0C)
+	time.sleep(0.5)
+	# MLX90393 address, 0x0C(12)
+
+	# Read data back from 0x4E(78), 7 bytes
+	# Status, xMag msb, xMag lsb, yMag msb, yMag lsb, zMag msb, zMag lsb
+	data = bus.read_i2c_block_data(0x0C, 0x4E, 7)
+
+	# Convert the data
+	xMag = data[1] * 256 + data[2]
+	if xMag > 32767 :
+  		xMag -= 65536
+	yMag = data[3] * 256 + data[4]
+	if yMag > 32767 :
+		yMag -= 65536
+	zMag = data[5] * 256 + data[6]
+	if zMag > 32767 :
+		zMag -= 65536
+
+	#Condition to threshold whether something is magnetic
+	threshold = 1000
+	if abs(zMag) > threshold or abs(yMag) > threshold or abs(xMag) > threshold:
+		Mag = YES
+	else Mag = NO
+
+	#Print values
+	print("MAGNETIC FIELD\t| ENVIRONMENTAL CONDITIONS\t|")
+	print("X-axis: %d\t|" %xMag + " Temperature: %0.1f C" %bme280.temperature)
+	print("Y-axis: %d\t|" %yMag + " Humidity: %0.1f %%" %bme280.humidity)
+	print("Z-axis: %d\t|" %zMag + " Pressure: %0.1f kPa" %(bme.pressure*0.1)")
+	print("Magnetic object: %d\t|\n" %Mag + " Altitude: %0.2f meters" %bme.280.altitude)
+
+	time.sleep(1)
